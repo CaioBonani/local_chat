b46d60709435d3d42a025cc574c7a84ce5d90e09
FernandoGT3 <luiz.moloni@gmail.com>
2023-09-03 15:29:15 -0300

    Teste

--- client/client_rust/src/main.rs
+++ client/client_rust/src/main.rs
@@ -1,6 +1,17 @@
 use tokio::net::TcpStream;
 use tokio::io::{AsyncReadExt, AsyncWriteExt};
 
+
+
+
+
+
+
+
+
+
+
+
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let addr = "127.0.0.1:8080";
